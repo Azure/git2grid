4136bd1002eebc207feb0989711542d820b82aa0
Jackie Elliott <t-jaelli@microsoft.com>
2018-06-13 12:59:30 -0700

    Handle github events in home.go

--- actions/home.go
+++ actions/home.go
@@ -1,9 +1,45 @@
 package actions
 
-import "github.com/gobuffalo/buffalo"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gobuffalo/buffalo"
+	"github.com/gobuffalo/buffalo/render"
+	"github.com/google/go-github/github"
+)
 
 // HomeHandler is a default handler to serve up
 // a home page.
 func HomeHandler(c buffalo.Context) error {
-	return c.Render(200, r.JSON(map[string]string{"message": "Welcome to Buffalo!"}))
+	request := c.Request()
+	payload, err := github.ValidatePayload(request, []byte(os.Getenv("APPSETTING_X_HUB_SIGNATURE")))
+	if err != nil {
+		log.Printf("secret key is not correct: err=%s\n", err)
+		return c.Error(http.StatusInternalServerError, err)
+	}
+	defer request.Body.Close()
+	event, err := github.ParseWebHook(github.WebHookType(request), payload)
+	if err != nil {
+		log.Printf("could not parse webhook: err=%s\n", err)
+		return c.Error(http.StatusInternalServerError, err)
+	}
+	repoName := ""
+	switch e := event.(type) {
+	case *github.PullRequestEvent:
+		if e.Action != nil {
+			repoName = *e.Repo.FullName
+			fmt.Printf("Repository Name: %s", *e.Repo.FullName)
+		}
+	case *github.LabelEvent:
+		if e.Action != nil {
+			repoName = *e.Repo.FullName
+		}
+	default:
+		log.Printf("unknown event type %s\n", github.WebHookType(request))
+		//return err
+	}
+	return c.Render(200, render.JSON(map[string]string{"message": "Welcome to Buffalo!", "repo name": repoName}))
 }
