ca6aba0d96e415dc4dc7753a01dfc37f4ca5c4f4
Jackie Elliott <t-jaelli@microsoft.com>
2018-06-19 15:44:14 -0700

    Removed the conversions dependency on event type

--- actions/transform.go
+++ actions/transform.go
@@ -1,10 +1,12 @@
 package actions
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/Azure/azure-sdk-for-go/services/eventgrid/2018-01-01/eventgrid"
@@ -16,7 +18,7 @@ import (
 	"github.com/google/go-github/github"
 )
 
-// TransformListen default implementation.
+// TransformListen converts a Github event into an Eventgrid event and published the event to Eventgrid
 func TransformListen(c buffalo.Context) error {
 	request := c.Request()
 	/*payload, err := github.ValidatePayload(request, []byte(os.Getenv("APPSETTING_X_HUB_SIGNATURE")))
@@ -27,65 +29,46 @@ func TransformListen(c buffalo.Context) error {
 	var err error
 	var body []byte
 	if body, err = ioutil.ReadAll(request.Body); err != nil {
-		//Should a request.Body.Close() go here?
 		return err
 	}
 	defer request.Body.Close()
 	event, err := github.ParseWebHook(github.WebHookType(request), body)
 	if err != nil {
-		log.Printf("could not parse webhook: err=%s\n", err)
+		log.Printf("Could not parse webhook: err=%s\n", err)
 		return c.Error(http.StatusInternalServerError, err)
 	}
-	//c.Logger().Debug("MADE IT")
-	//repoName := ""
-	var myEvent eventgrid.Event
-	//can take address of myDate directly because it is a local variable
 	myDate := date.Time{Time: time.Now()}
-	var events []eventgrid.Event
+
 	var myClient = eventgrid.New()
 	myClient.Authorizer = autorest.NewEventGridKeyAuthorizer(os.Getenv("APPSETTING_TOPIC_KEY"))
-	switch e := event.(type) {
-	case *github.PullRequestEvent:
-		if e.Action != nil {
-			//repoName = *e.Repo.FullName
-			myEvent = eventgrid.Event{
-				EventType:       to.StringPtr(os.Getenv("APPSETTING_EVENT_TYPE")),
-				EventTime:       &myDate,
-				ID:              to.StringPtr(os.Getenv("APPSETTING_ID")),
-				Data:            e,
-				Subject:         e.PullRequest.URL,
-				DataVersion:     to.StringPtr("1"),
-				MetadataVersion: to.StringPtr("1"),
-			}
-			events = append(events, myEvent)
-			result, err := myClient.PublishEvents(request.Context(), os.Getenv("APPSETTING_TOPIC_HOSTNAME"), events)
-			if err != nil {
-				log.Printf("Could not publish pull request event to event grid: err=%s\n", err)
-				return c.Error(result.Response.StatusCode, err)
-			}
-		}
-	case *github.LabelEvent:
-		if e.Action != nil {
-			//repoName = *e.Repo.FullName
-			myEvent = eventgrid.Event{
-				EventType:       to.StringPtr("Github.LabelEvent"),
-				EventTime:       &myDate,
-				ID:              to.StringPtr(os.Getenv("APPSETTING_ID")),
-				Data:            e,
-				Subject:         e.Label.URL,
-				DataVersion:     to.StringPtr("11"),
-				MetadataVersion: to.StringPtr("1"),
-			}
-			events = append(events, myEvent)
-			result, err := myClient.PublishEvents(request.Context(), os.Getenv("APPSETTING_TOPIC_HOSTNAME"), events)
-			if err != nil {
-				log.Printf("Could not publish label event to event grid: err=%s\n", err)
-				return c.Error(result.Response.StatusCode, err)
-			}
-		}
-	default:
-		log.Printf("unknown event type %s\n", github.WebHookType(request))
-		//return err
+	eventName := formatEventName(github.WebHookType(request))
+
+	eventType := fmt.Sprintf("Github.%s", eventName)
+
+	myEvent := eventgrid.Event{
+		EventType:       to.StringPtr(eventType),
+		EventTime:       &myDate,
+		ID:              to.StringPtr(os.Getenv("APPSETTING_ID")),
+		Data:            event,
+		Subject:         to.StringPtr("GitToGrid"),
+		DataVersion:     to.StringPtr("1"),
+		MetadataVersion: to.StringPtr("1"),
+	}
+	var events []eventgrid.Event
+	events = append(events, myEvent)
+	result, err := myClient.PublishEvents(request.Context(), os.Getenv("APPSETTING_TOPIC_HOSTNAME"), events)
+	if err != nil {
+		log.Printf("Could not publish %s event to event grid: err=%s\n", eventName, err)
+		return c.Error(result.Response.StatusCode, err)
 	}
 	return c.Render(200, render.JSON([]eventgrid.Event{myEvent}))
+}
+
+func formatEventName(name string) string {
+	sepName := strings.Split(name, "_")
+	for i, v := range sepName {
+		sepName[i] = strings.Title(v)
+	}
+	sepName = append(sepName, "Event")
+	return strings.Join(sepName, "")
 }
