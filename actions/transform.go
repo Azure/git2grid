da9e1ec6780bd633934029f068da3d35904f5a7a
Billy Trend <witrend@microsoft.com>
2018-10-16 19:40:11 -0700

    Puts handler in working state

--- actions/transform.go
+++ actions/transform.go
@@ -1,12 +1,10 @@
 package actions
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"strings"
 	"time"
 
 	"github.com/Azure/azure-sdk-for-go/services/eventgrid/2018-01-01/eventgrid"
@@ -16,16 +14,18 @@ import (
 	"github.com/gobuffalo/buffalo"
 	"github.com/gobuffalo/buffalo/render"
 	"github.com/google/go-github/github"
+	"github.com/google/uuid"
 )
 
 // TransformListen converts a Github event into an Eventgrid event and publishes the event to Eventgrid
 func TransformListen(c buffalo.Context) error {
+
 	request := c.Request()
-	/*payload, err := github.ValidatePayload(request, []byte(os.Getenv("APPSETTING_X_HUB_SIGNATURE")))
-	if err != nil {
-		log.Printf("secret key is not correct: err=%s\n", err)
-		return c.Error(http.StatusInternalServerError, err)
-	}*/
+	// payload, err := github.ValidatePayload(request, []byte(os.Getenv("APPSETTING_X_HUB_SIGNATURE")))
+	// if err != nil {
+	// 	log.Printf("secret key is not correct: err=%s\n", err)
+	// 	return c.Error(http.StatusInternalServerError, err)
+	// }
 	var err error
 	var body []byte
 	if body, err = ioutil.ReadAll(request.Body); err != nil {
@@ -33,42 +33,38 @@ func TransformListen(c buffalo.Context) error {
 	}
 	defer request.Body.Close()
 	event, err := github.ParseWebHook(github.WebHookType(request), body)
+
 	if err != nil {
 		log.Printf("Could not parse webhook: err=%s\n", err)
 		return c.Error(http.StatusInternalServerError, err)
 	}
-	myDate := date.Time{Time: time.Now()}
 
 	var myClient = eventgrid.New()
-	myClient.Authorizer = autorest.NewEventGridKeyAuthorizer(os.Getenv("APPSETTING_TOPIC_KEY"))
-	eventName := FormatEventName(github.WebHookType(request))
+	myClient.Authorizer = autorest.NewEventGridKeyAuthorizer(os.Getenv("eventgridSecret"))
 
-	eventType := fmt.Sprintf("Github.%s", eventName)
+	var now = date.Time{Time: time.Now()}
+	var uuidBytes, _ = uuid.NewRandom()
+	var uuidString = string(uuidBytes[:16])
 
 	myEvent := eventgrid.Event{
-		EventType:       to.StringPtr(eventType),
-		EventTime:       &myDate,
-		ID:              to.StringPtr(os.Getenv("APPSETTING_ID")),
+		EventType:       to.StringPtr(github.WebHookType(request)),
+		EventTime:       &now,
+		ID:              to.StringPtr(uuidString),
 		Data:            event,
 		Subject:         to.StringPtr("GitToGrid"),
 		DataVersion:     to.StringPtr("1"),
 		MetadataVersion: to.StringPtr("1"),
 	}
+
 	var events []eventgrid.Event
 	events = append(events, myEvent)
-	result, err := myClient.PublishEvents(request.Context(), os.Getenv("APPSETTING_TOPIC_HOSTNAME"), events)
+
+	result, err := myClient.PublishEvents(request.Context(), "gridfromgit.westus2-1.eventgrid.azure.net", events)
+
 	if err != nil {
-		log.Printf("Could not publish %s event to event grid: err=%s\n", eventName, err)
+		log.Printf("Could not publish %s event to event grid: err=%s\n", github.WebHookType(request), err)
 		return c.Error(result.Response.StatusCode, err)
 	}
-	return c.Render(200, render.JSON([]eventgrid.Event{myEvent}))
-}
 
-func FormatEventName(name string) string {
-	sepName := strings.Split(name, "_")
-	for i, v := range sepName {
-		sepName[i] = strings.Title(v)
-	}
-	sepName = append(sepName, "Event")
-	return strings.Join(sepName, "")
+	return c.Render(200, render.JSON([]eventgrid.Event{myEvent}))
 }
